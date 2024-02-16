4470009abeb976004323f8cb054077e68513e6cc
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2024-02-16 10:39:35 -0300

    useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState({ count: 0, theme: "light" });
+
+  // Um hook do react não pode ser chamado condicionalmente, a quantidade de uso deve sempre ser a mesma, sendo assim invariante
+  // if (count === 1){
+  //   useState()
+  // }
+
+  const incrementCount = () => {
+    setCount((prev) => {
+      return {
+        ...prev,
+        count: prev.count + 1,
+      };
+    });
+  };
+
   return (
-   <div>
-    
-   </div>
+    <div>
+      <h1>{count.count}</h1>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
   );
 }
